d6a9a31efe898ca91491939954514609a04da140
wkaczurba <witold.kaczurba@gmail.com>
2017-01-06 07:35:16 +0000

    Simple webflow with two views; working ok.

---
+++ SpringWebflow/src/main/java/com/springpizza/HomeController.java
@@ -0,0 +1,13 @@
+package com.springpizza;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String main() {
+		return "redirect://pizza";
+	}
+}
